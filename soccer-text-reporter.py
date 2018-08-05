19a6532da93f368774c9c3f6ee0d7900e03c8f14
upura <upura0@gmail.com>
2018-08-05 13:26:28 +0900

    get goal flg

--- soccer-text-reporter.py
+++ soccer-text-reporter.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
+
 URL = 'https://www.jleague.jp/match/j1/2018/080103/livetxt/'
 
 def get_data():
@@ -14,6 +15,10 @@ def get_data():
     text_list = driver.find_elements_by_class_name('spotRightTxt')
     texts = [text.text for text in text_list]
 
+    # ゴール判定
+    goal_list = driver.find_elements_by_class_name('spotRightTxt')
+    goals = [1 if '<img class=' in goal.get_attribute("innerHTML") else 0 for goal in goal_list]
+
     # 時刻
     time_list = driver.find_elements_by_class_name('spotLeftTxt')
     times = [time.text for time in time_list]
@@ -25,14 +30,20 @@ def get_data():
     driver.close()
     driver.quit()
 
-    print(texts[0])
-    print(times[0])
-    print(teams[0])
-    print(len(texts))
-    print(len(times))
-    print(len(teams))
+    # with open("texts.txt", 'wt') as f:
+    #     for ele in texts:
+    #         f.write(ele+'\n')
+    # with open("goals.txt", 'wt') as f:
+    #     for ele in goals:
+    #         f.write(str(ele)+'\n')
+    # with open("times.txt", 'wt') as f:
+    #     for ele in times:
+    #         f.write(ele+'\n')
+    # with open("teams.txt", 'wt') as f:
+    #     for ele in teams:
+    #         f.write(ele+'\n')
 
-    return texts, times, teams
+    return texts, goals, times, teams
 
 if __name__ == '__main__':
-    texts, times, teams = get_data()
+    texts, goals, times, teams = get_data()
